46dd53efd36d2999e519e14755c74a06830359d0
Wahab Sheikh <wahabmsheikh@gmail.com>
2014-12-15 13:58:20 -0800

    Add ability to make and print grid

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,10 +1,12 @@
+require 'byebug'
+
 class Tile
 
   attr_accessor :position, :state, :board
 
-  def initialize(position, state, board = Board.new)
+  def initialize(position, state, board)
     @position = position
-    @state = state # bombed, flagged, revealed
+    @state = state #
     @board = board
   end
 
@@ -18,7 +20,7 @@ class Tile
   end
 
   def inspect
-    "Position: #{position}, State: #{state}"
+    #{}"Position: #{position}, State: #{state}"
   end
 
 end
@@ -27,19 +29,26 @@ class Board
 
   attr_accessor :grid
 
-  def initialize(grid_size = 9)
-    @grid = make_grid(grid_size)
+  # def self.new_board
+  #   b = Board.new
+  #   b.make_grid(9)
+  #   b
+  # end
+
+  def initialize
+    #@grid = Array.new(9) { Array.new(9) }
+    make_grid(9)
   end
 
   def [](pos)
-    grid[pos[0], pos[1]]
+    grid[pos[0]][pos[1]]
   end
 
   def []=(pos, mark)
-    self.grid[pos[0], pos[1]] = mark
+    self.grid[pos[0]][pos[1]] = mark
   end
 
-  def print_board
+  def display_board
     grid.each do |row|
       row.each do |tile|
         p tile
@@ -48,17 +57,39 @@ class Board
   end
 
 
-  private
-
-    def make_grid(grid_size)
-      self.grid = Array.new(grid_size) { Array.new(grid_size) }
-      (0..grid_size).each do |r|
-        (0..grid_size).each do |c|
-          self[[r, c]] = Tile.new([r, c], nil, self)
-        end
+  def make_grid(grid_size)
+    self.grid = Array.new(grid_size) { Array.new(grid_size) }
+    (0...grid_size).each do |r|
+      (0...grid_size).each do |c|
+        self[[r, c]] = Tile.new([r, c], :none, self)
       end
-      self.grid
     end
+    seed_bombs(5)
+  end
+
+  def seed_bombs(num)
+    num.times do
+      self[[rand(8), rand(8)]].state = :bomb
+    end
+  end
 
 
+  def play
+
+    puts "Enter coordinates (add -f to flag)"
+    action = gets.chomp.split(" ")
+      if action.include?("f")
+        #flag
+      else
+        #reveal
+      end
+
+  end
 end
+
+
+
+b = Board.new
+
+#b[[3,4]] = Tile.new([3,4], :none, b)
+b.display_board
